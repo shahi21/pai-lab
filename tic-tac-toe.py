397b3b1c4d243ff1e88b9aa4b303304286d31c61
Shahistha <shahisthanavab@gmail.com>
2024-01-30 18:50:33 +0530

    commit

---
+++ tic-tac-toe.py
@@ -0,0 +1,55 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_board_full(board):
+    return all(board[i][j] != " " for i in range(3) for j in range(3))
+
+def get_player_input(player):
+    while True:
+        try:
+            move = int(input(f"Player {player}, enter your move (1-9): "))
+            if 1 <= move <= 9:
+                return (move - 1) // 3, (move - 1) % 3
+            else:
+                print("Invalid input. Please enter a number between 1 and 9.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+
+        row, col = get_player_input(current_player)
+
+        if board[row][col] == " ":
+            board[row][col] = current_player
+
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+            else:
+                current_player = "O" if current_player == "X" else "X"
+        else:
+            print("Cell already taken. Try again.")
+
+if __name__ == "__main__":
+    tic_tac_toe()
